7bfb4ab42cb9187810ef9ef0b624a02b61012973
Vyacheslav Goryachkin <vgoryachkin@kameleoon.com>
2021-05-10 23:49:14 +0300

    Create routes and controllers

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-require('dotenv').config();
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
+const fileupload = require('express-fileupload')
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleWare')
 
-const express = require('express');
-const sequelize = require('./db');
-const PORT = process.env.PORT || 5000;
-const models = require('./models/models'); 
-const cors = require('cors');
+const PORT = process.env.PORT || 5000
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 app.use(express.json())
+app.use(fileupload({}))
+app.use('/api', router)
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: "Its working!"})
-})
+// Обработчик ошибок, последний Middleware
+app.use(errorHandler)
 
 const start = async () => {
   try {
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () => console.log(`Server has been started on port ${PORT}`));
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () =>
+      console.log(`Server has been started on port ${PORT}`)
+    )
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
 }
 
-start();
+start()
